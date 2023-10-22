76e1bc6299363f8abe6b3c92accff5ed7d7e7bd5
Lucas Alves <lucas.arnaldo@gmail.com>
2023-10-22 02:22:36 -0300

    Aula 3 imersão15

---
+++ codepix/application/factory/factory.go
@@ -0,0 +1,20 @@
+package factory
+
+import (
+	"codepix-go/application/usecase"
+	"codepix-go/infrastructure/repository"
+
+	"github.com/jinzhu/gorm"
+)
+
+func TransactionUseCaseFactory(database *gorm.DB) usecase.TransactionUseCase {
+	pixRepository := repository.PixKeyRepositoryDb{Db: database}
+	transactionRepository := repository.TransactionRepositoryDb{Db: database}
+
+	transactionUseCase := usecase.TransactionUseCase{
+		TransactionRepository: &transactionRepository,
+		PixRepository:         pixRepository,
+	}
+
+	return transactionUseCase
+}
